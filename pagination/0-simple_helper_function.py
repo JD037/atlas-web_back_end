ed2cc6e41cab135f3588caa7e6233baa8c2d0cf9
Josh D <joj237@gmail.com>
2024-01-09 16:05:35 -0600

    fix helper function and add csv file

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+"""
+Write a function named index_range that takes two integer arguments
+page and page_size.
+"""
+
+from typing import Tuple
+
+
 def index_range(page, page_size):
     """
     Return a tuple of size two containing a start index and an end index
