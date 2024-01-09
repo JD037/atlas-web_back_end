20bced39247acb35ca665e27b356121edee88eb5
Josh D <joj237@gmail.com>
2024-01-09 17:14:17 -0600

    document index range func

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -8,6 +8,11 @@ from typing import List, Dict
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
     end_index = page * page_size
     start_index = end_index - page_size
     return (start_index, end_index)
