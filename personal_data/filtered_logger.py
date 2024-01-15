35693658c4f4eaa7d500101212248879a1dbeb92
Josh D <joj237@gmail.com>
2024-01-15 16:47:28 -0600

    fix python issue with import mysql

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,8 +3,10 @@
 Filtered logger module
 """
 
+import os
 import re
 import logging
+import mysql.connector
 from typing import List
 
 
@@ -80,3 +82,24 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Create and return a connector to the database.
+
+    Returns:
+        MySQLConnection: The connection to the database.
+    """
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    connection = mysql.connector.connect(
+        user=username, 
+        password=password, 
+        host=host, 
+        database=db_name
+    )
+    return connection
