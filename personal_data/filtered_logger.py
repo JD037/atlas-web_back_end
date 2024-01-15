a3a2e87854f15d924901256db33ea84a7dc0c386
Josh D <joj237@gmail.com>
2024-01-15 17:38:29 -0600

    task 4 setup main

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -101,3 +101,24 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         user=username, password=password, host=host, database=db_name
     )
     return connection
+
+
+def main():
+    """
+    Main function that retrieves and displays filtered
+    user data.
+    """
+    db_connection = get_db()
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+
+    for row in cursor.fetchall():
+        to_log = ("name={};email={};phone={};ssn={};password={};"
+                  "ip={};last_login={};user_agent={};").format(*row)
+        logger.info(filter_datum(PII_FIELDS, "***", to_log, ";"))
+
+    cursor.close()
+    db_connection.close()
+    if name == "main":
+        main()
