bf0a68e7937fc59766ae78bac0e061ab1377c519
Josh D <joj237@gmail.com>
2024-01-17 00:11:47 +0000

    add extract base 64 method to basic auth class

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,14 @@ class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth.
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header.
+        """
+        if authorization_header is None or \
+           not isinstance(authorization_header, str) or \
+           not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split(' ')[1]
