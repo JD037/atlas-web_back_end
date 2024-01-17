edb2e4cbfee989b30222433af7075324339cd623
Josh D <joj237@gmail.com>
2024-01-17 00:17:18 +0000

    revert pycodestyle changes to find new issue

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,12 +10,10 @@ class BasicAuth(Auth):
     BasicAuth class that inherits from Auth.
     """
 
-    def extract_base64_authorization_header(
-        self, authorization_header: str
-	) -> str:
-		"""
-		Extracts the Base64 part of the Authorization header.
-		"""
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header.
+        """
         if authorization_header is None or \
            not isinstance(authorization_header, str) or \
            not authorization_header.startswith('Basic '):
