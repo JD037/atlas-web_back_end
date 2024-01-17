55adca5e23387729fb042b95b22a4ea13f4431cc
Josh D <joj237@gmail.com>
2024-01-17 00:34:53 +0000

    revert changes

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,11 +61,7 @@ class BasicAuth(Auth):
         credentials = decoded_base64_authorization_header.split(':', 1)
         return credentials[0], credentials[1]
 
-    def user_object_from_credentials(
-        self,
-        user_email: str,
-        user_pwd: str
-    ) -> TypeVar('User'):
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """
         Returns the User instance based on his email and password.
         """
