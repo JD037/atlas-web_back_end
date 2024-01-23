2d29422ceffda1fe3687ef7425568775603e3450
Josh D <joj237@gmail.com>
2024-01-23 22:37:10 +0000

    integrate login functionality

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -21,6 +21,24 @@ def users():
     except ValueError:  # User already exists
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Handle POST request for user login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        abort(401)  # Unauthorized if email or password missing
+
+    if not AUTH.valid_login(email, password):
+        abort(401)  # Unauthorized if invalid login
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
