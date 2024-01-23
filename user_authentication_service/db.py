2aeec604775edc2c65c6c62fc4a90b9fdab0954a
Josh D <joj237@gmail.com>
2024-01-23 20:40:45 +0000

    add update user method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -65,3 +65,24 @@ class DB:
             return user
         except InvalidRequestError:
             raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user's attributes.
+        
+        Args:
+            user_id (int): The ID of the user to update.
+            **kwargs: Arbitrary keyword arguments containing the attributes to update.
+        
+        Raises:
+            ValueError: If an attribute to update does not correspond to a user attribute.
+        """
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+
+        self._session.commit()
