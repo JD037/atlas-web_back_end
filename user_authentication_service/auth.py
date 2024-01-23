027abf41b70eb84897e80a2dda9a1080420b0178
Josh D <joj237@gmail.com>
2024-01-23 22:07:08 +0000

    add valid login method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -24,6 +24,26 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validate user login credentials.
+
+        Args:
+            email (str): The user's email address.
+            password (str): The user's password.
+
+        Returns:
+            bool: True if valid login, else False.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        return bcrypt.checkpw(
+            password.encode('utf-8'),
+            user.hashed_password.encode('utf-8'))
+
     def register_user(self, email: str, password: str) -> User:
         """
         Registers a new user with an email and password.
