ed93773b0ba2fc96a99defbf622c30a4e880e912
Josh D <joj237@gmail.com>
2024-01-23 18:58:20 +0000

    update session auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 """
-This module contains the SessionAuth class
+SessionAuth class for managing user session authentication
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     SessionAuth class that inherits from Auth.
-    At the moment, this class is empty and serves as a placeholder
-    for future implementation of session-based authentication.
+    Manages sessions of users.
     """
 
-    def __init__(self):
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
         """
-        Initialize the SessionAuth instance
+        Creates a session ID for a user_id and returns the session ID.
         """
-        super().__init__()
+        if user_id is None or type(user_id) is not str:
+            return None
+        
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
