257504e5b2a9b60b16b8c969743f2d7577db1b21
Josh D <joj237@gmail.com>
2024-01-30 20:40:14 +0000

    documentation and pycodestyle task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Get Locale from Request"""
+"""Get Locale from Request - Sets up a Flask app
+and configures Babel for internationalization."""
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
@@ -7,7 +8,14 @@ app = Flask(__name__)
 
 
 class Config:
-    """Configuration for Babel"""
+    """Configuration class for Babel.
+
+    Attributes:
+        LANGUAGES (list): a list of languages supported by the application.
+        BABEL_DEFAULT_LOCALE (str): the default locale used by the application.
+        BABEL_DEFAULT_TIMEZONE (str): the default timezone used
+        by the application.
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -19,13 +27,21 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """Select a language translation to use for each request."""
+    """Determine the best match language for the user's preferences.
+
+    Returns:
+        str: the best match language from the user's preferences.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
-    """Renders the HTML page when requested"""
+    """Render the main page of the website.
+
+    Returns:
+        str: HTML content of the main page.
+    """
     return render_template('2-index.html')
 
 
