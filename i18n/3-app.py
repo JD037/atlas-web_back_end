fa5ab745759c27d9796d7599c8f85793c9683bd5
Josh D <joj237@gmail.com>
2024-01-31 17:26:22 +0000

    test task 3 again

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,60 +1,39 @@
 #!/usr/bin/env python3
-"""Get Locale from Request - Sets up a Flask app
-and configures Babel for internationalization."""
+""" Create a basic Flask App
+    with a single '/' route and an index.html template
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
-from flask_babel import get_locale 
+
 
 app = Flask(__name__)
+# Instantiate Babel object in module-level variable babel
+babel = Babel(app)
 
 
-class Config:
-    """Configuration class for Babel.
-
-    Attributes:
-        LANGUAGES (list): a list of languages supported by the application.
-        BABEL_DEFAULT_LOCALE (str): the default locale used by the application.
-        BABEL_DEFAULT_TIMEZONE (str): the default timezone used
-        by the application.
-    """
+class Config():
+    """ Configure available languages in our app """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Use Config class as config for our app
 app.config.from_object(Config)
-babel = Babel(app)
-
-@app.context_processor
-def inject_locale():
-    return dict(current_locale=str(get_locale()))
 
 
 @babel.localeselector
 def get_locale():
-    """Determine the best match language for the user's preferences.
-
-    Returns:
-        str: the best match language from the user's preferences.
-    """
-    user_locale = request.args.get('locale')
-    print(f"Requested locale: {user_locale}")  # Debug print
-    if user_locale in app.config['LANGUAGES']:
-        return user_locale
-    best_match = request.accept_languages.best_match(app.config['LANGUAGES'])
-    print(f"Best match: {best_match}")  # Debug print
-    return best_match
+    """ Return user preferred locale, if not available return best match """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """Render the main page of the website.
-
-    Returns:
-        str: HTML content of the main page.
-    """
-    return render_template('4-index.html')
+    """ Return index.html template """
+    from flask_babel import gettext as _
+    return render_template('/3-index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
