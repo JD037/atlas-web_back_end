bd507c54ea2dae848b64d0a31ab777e04ebc6185
Josh D <joj237@gmail.com>
2024-02-13 17:21:25 -0600

    fix sql output for task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- Ranks country origins of bands by the total number of fans
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM bands
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+WHERE split IS NULL -- Considering only bands that haven't split
 GROUP BY origin
 ORDER BY nb_fans DESC;
