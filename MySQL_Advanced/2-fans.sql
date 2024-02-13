d9b9f213c7cb656abd585613a4d7f775585dfb84
Josh D <joj237@gmail.com>
2024-02-13 17:24:43 -0600

    change sql query omit split bands

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
 -- Ranks country origins of bands by the total number of fans
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
-WHERE split IS NULL -- Considering only bands that haven't split
 GROUP BY origin
 ORDER BY nb_fans DESC;
