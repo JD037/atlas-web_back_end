780251939a55a9e0ab2761f0be13cb0deb4e8ba4
Josh D <joj237@gmail.com>
2024-02-13 17:35:25 -0600

    revert back didnt work

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,9 +1,13 @@
+-- Trigger to decrease item quantity on new order
 DELIMITER $$
+
 CREATE TRIGGER after_order_insert
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    UPDATE items SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
+    UPDATE items
+    SET quantity = quantity - NEW.number_ordered
+    WHERE id = NEW.item_id;
 END$$
+
 DELIMITER ;
