4907dbb1a06e679af3221af930de040320d258c7
Josh D <joj237@gmail.com>
2024-02-14 10:28:12 -0600

    fix scripts 8 and 9

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 # 8-all.py
+
 def list_all(mongo_collection):
     """Function that lists all documents in a collection"""
     return list(mongo_collection.find())
--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
 # 9-insert_school.py
 def insert_school(mongo_collection, **kwargs):
+    """Function that inserts a new document in a collection based on kwargs"""
     return mongo_collection.insert_one(kwargs).inserted_id
