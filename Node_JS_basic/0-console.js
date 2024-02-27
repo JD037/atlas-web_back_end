f89eb98fa684dc0d2cfb27adc91d38230d5cff79
Josh D <joj237@gmail.com>
2024-02-27 22:32:00 +0000

    repo setup

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+// Task 0 - Create a function named display Message that
+// prints in STDOUT the string arg
+
+const displayMessage = (message) => {
+	console.log(message);
+  };
+  
+  export default displayMessage;
