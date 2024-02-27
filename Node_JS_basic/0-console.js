e678cc570f04353e5efa805e6ab2d5c042089ad7
Josh Davis <120410314+JD037@users.noreply.github.com>
2024-02-27 11:16:36 -0600

    Delete Node_JS_basic directory
    messed up environment and staring over

--- Node_JS_basic/0-console.js
+++
@@ -1,8 +0,0 @@
-// Task 0 - Create a function named display Message that
-// prints in STDOUT the string arg
-
-const displayMessage = (message) => {
-	console.log(message);
-  };
-  
-  export default displayMessage;
