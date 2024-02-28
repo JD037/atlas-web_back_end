bce39592ab17f748e299aa14f094bc9418365288
Josh D <joj237@gmail.com>
2024-02-28 23:41:49 +0000

    still broken

---
+++ queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,29 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account',
+  },
+  // Add more job objects as needed
+];
+
+jobs.forEach((jobData) => {
+  const job = queue.create('push_notification_code_2', jobData).save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+  });
+
+  job.on('complete', () => {
+    console.log(`Notification job ${job.id} completed`);
+  });
+
+  job.on('failed', (err) => {
+    console.log(`Notification job ${job.id} failed: ${err}`);
+  });
+
+  job.on('progress', (progress) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
+});
